555c6a874a9987bcf67d844fd9d6971d0ec7e760
NiaAxern <gdmixerno@gmail.com>
2024-02-24 09:42:32 +0200

    Added throttle and graph options to config.ts

--- config.ts
+++ config.ts
@@ -7,6 +7,12 @@ export default {
 		port: null, // Any port between 1-65535 or null to make it random.
 	},
 	bot: {
+		throttle: {
+			ms: 10000, // waits for that many seconds before it sends the api message so that it doesn't start spamming
+			max: null, // max subscribers to have throttle on (null == no limit)
+			min: null // min subscribers to have throttle on (null == no limit)
+		},
+		graphs: true,
 		privateMessages: true, // allow tracking to be done in private messages
 	},
 	youtube: {
